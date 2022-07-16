edf36318cc89ecf85ed0e2c7eb67ae681a642e58
amulshindeIN <66879770+amulshindeIN@users.noreply.github.com>
2022-07-16 14:08:38 +0530

    added username

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: 
+   # <your GitHub username> : amulshindeIN
 
    # Input:
    # p, principal amount
